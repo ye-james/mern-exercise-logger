3f9932fd4ad94aae9ea399811e55335d25195b57
ksy415 <sykevin415@gmail.com>
2021-06-12 19:44:11 -0700

    Add signup button to nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Menu, Segment, Input, Button } from 'semantic-ui-react';
@@ -8,7 +8,7 @@ import { logoutUser } from '../redux/actions/user';
 
 const NavBar = () => {
   const dispatch = useDispatch();
-  const [activeItem, setActiveItem] = useState('home')
+  const [activeItem, setActiveItem] = useState('home');
   const loggedIn = useSelector(state => state.user.isAuthenticated);
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
   const history = useHistory();
@@ -16,70 +16,78 @@ const NavBar = () => {
   //console.log(user);
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem('profile')));
-  })
+  });
 
   const handleItemClick = ({ name }) => {
     setActiveItem(name);
-  }
- 
-  const handleLogout = () => {
-      setUser(false);
-      dispatch(logoutUser(history));
-  }
+  };
 
-    return (
-      <Segment basic inverted style={{marginBottom: '0'}}>
-        <Menu inverted pointing secondary>
+  const handleLogout = () => {
+    setUser(false);
+    dispatch(logoutUser(history));
+  };
+
+  return (
+    <Segment basic inverted style={{ marginBottom: '0' }}>
+      <Menu inverted pointing secondary>
+        <Menu.Item
+          as={Link}
+          to='/'
+          name='home'
+          active={activeItem === 'home'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          as={Link}
+          to='explore'
+          name='explore'
+          active={activeItem === 'explore'}
+          onClick={handleItemClick}
+        />
+        {user ? (
           <Menu.Item
-            as={Link}
-            to='/'
-            name='home'
-            active={activeItem === 'home'}
-            onClick={handleItemClick}
-  
-          />
-          <Menu.Item
-            as={Link}
-            to='explore'          
-            name='explore'
-            active={activeItem === 'explore'}
-            onClick={handleItemClick}
-          />
-          {user? <Menu.Item
             as={Link}
             to={`log/${user.id}`}
             name='log'
             active={activeItem === 'log'}
             onClick={handleItemClick}
-          /> : null}
-          <Menu.Item
-            name='progress'
-            as={Link}
-            to='progress'
-            active={activeItem === 'Progress'}
-            onClick={handleItemClick}
           />
-          <Menu.Menu position='right'>
-            <Menu.Item>
-              <Input icon='search' placeholder='Search...' />
-            </Menu.Item>
-            {loggedIn  && user && <Menu.Item name={`Welcome ${user.name.split(' ')[0]}`}/>}
-            {loggedIn ? <Menu.Item
+        ) : null}
+        <Menu.Item
+          name='progress'
+          as={Link}
+          to='progress'
+          active={activeItem === 'Progress'}
+          onClick={handleItemClick}
+        />
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search...' />
+          </Menu.Item>
+          {loggedIn && user ? (
+            <Menu.Item name={`Welcome ${user.name.split(' ')[0]}`} />
+          ) : (
+            <Menu.Item name='sign up' as={Link} to='/user/signup' />
+          )}
+          {loggedIn ? (
+            <Menu.Item
               name='logout'
               active={activeItem === 'logout'}
               as={Button}
               onClick={handleLogout}
-            /> : <Menu.Item
-            name='login'
-            active={activeItem === 'login'}
-            as={Link}
-            to='/user/login'
-          /> }
-          </Menu.Menu>          
-        </Menu>
-      </Segment>
-    )
-}
+            />
+          ) : (
+            <Menu.Item
+              name='login'
+              active={activeItem === 'login'}
+              as={Link}
+              to='/user/login'
+            />
+          )}
+        </Menu.Menu>
+      </Menu>
+    </Segment>
+  );
+};
 
 export default NavBar;
-
