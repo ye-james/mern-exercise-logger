650c48490abd18f84eeaae95d8d74af104ebed2e
ksy415 <sykevin415@gmail.com>
2021-06-18 14:29:06 -0700

    Add validation to /login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,57 +1,81 @@
-require('dotenv').config(); 
-const mongoose = require('mongoose');
+require('dotenv').config();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 
+exports.loginUser = async (req, res) => {
+  // get the errors from the validation middleware
+  const errors = validationResult(req);
 
-exports.loginUser = async(req, res)=> {
-    try {
-        if ( !(req.body.user.username || req.body.user.password)) {
-            res.sendStatus(403).json({message: 'Missing username or password'})
-        }
-        const userExist = await User.findOne({username: req.body.user.username})
-        if(userExist) {
-            const validPassword = await bcrypt.compare(req.body.user.password, userExist.password);
-            if(validPassword) {
-                const token = jwt.sign({email: userExist.email, id: userExist._id}, process.env.TOKEN_SECRET, {expiresIn: '1hr'});
-                res.status(200).json({
-                    message: 'Successfully logged in!',
-                    data: {
-                        id: userExist._id, 
-                        name: userExist.name,
-                        token: token}
-                })
-            }
-            else {
-                res.status(400).json({message: 'Incorrect password'})
-            }
-        } 
-    } catch (error) {
-        res.status(500).json({error: error.message})
+  // return 400 if validation failed
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    // see if the user exists
+    const user = await User.findOne({ username });
+
+    // return 400 if user doesn't exists in db
+    if (!user) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: 'Invalid username or password.',
+            },
+          ],
+        });
+    };
+
+    // check if password is correct
+    const validPassword = await bcrypt.compare(
+        password,
+        user.password
+    );
+    
+    // return 400 if password is incorrect
+    if (!validPassword) {
+        return res.status(400).json({
+            errors: [
+                {
+                msg: 'Invalid username or password.',
+                },
+            ],
+        });
     }
-}
 
+    // create json web token
+    const token = jwt.sign({email: userExist.email, id: userExist._id}, process.env.TOKEN_SECRET, {expiresIn: '1hr'});
+    res.status(200).json({
+        message: 'Successfully logged in!',
+        data: {
+            id: userExist._id, 
+            name: userExist.name,
+            token: token}
+    })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error');
+  }
+};
 
-exports.signupUser = async(req, res)=> {
-    try{
-        const userExist = await User.exists({username: req.body.user.username})
-        if(userExist) {
-            res.status(409).json('Conflict: username already exists');
-        } else {
-            const hashedPassword = await bcrypt.hash(req.body.user.password, 10);
-            const newUser = new User({...req.body.user, password: hashedPassword});
-            newUser.save().
-            then(result => {
-                res.status(201).json({message: 'Successfully created user'})
-            })
-        }
-    } catch (error) {
-        res.status(500).send(error.message)
+exports.signupUser = async (req, res) => {
+  try {
+    const userExist = await User.exists({ username: req.body.user.username });
+    if (userExist) {
+      res.status(409).json('Conflict: username already exists');
+    } else {
+      const hashedPassword = await bcrypt.hash(req.body.user.password, 10);
+      const newUser = new User({ ...req.body.user, password: hashedPassword });
+      newUser.save().then(result => {
+        res.status(201).json({ message: 'Successfully created user' });
+      });
     }
-}
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
-
-function generateAccessToken(username) {
-    return jwt.sign(username, process.env.TOKEN_SECRET, {expiresIn: '3600s'})
-}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user')
-
+const userController = require('../controllers/user');
+const { check } = require('express-validator');
 
 router.post('/signup', userController.signupUser);
-router.post('/login', userController.loginUser);
+
+router.post(
+  '/login',
+  [
+    check('username', 'Username is required').exists(),
+    check('password', 'Password is required').exists(),
+  ],
+  userController.loginUser
+);
 
 module.exports = router;
