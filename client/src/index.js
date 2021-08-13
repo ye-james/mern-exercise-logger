835e8146b2c5b3880375b8fab7b898be8799eefb
ksy415 <sykevin415@gmail.com>
2021-08-12 17:49:47 -0700

    Move redux store into its own file

--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore , applyMiddleware, compose} from 'redux';
-import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
-import { PersistGate } from 'redux-persist/integration/react'
+
 import './index.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 
-import rootReducer from './redux/reducers';
-import { BrowserRouter } from 'react-router-dom';
-
-
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()), );
-const persistor = persistStore(store);
-
-ReactDOM.render(
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </PersistGate>
-      </Provider>
-,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
